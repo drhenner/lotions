79610d267edbfd7c47b7d6daf96a27b67b660a76
David Henner <davidhenner@drhenner.local>
2011-01-03 07:59:52 +0000

    remove amount that is taken from the user's store_credit from their order

--- app/models/store_credit.rb
+++ app/models/store_credit.rb
@@ -6,4 +6,12 @@ class StoreCredit < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :amount , :presence => true
 
+  # removes amount from object using SQL math
+  #
+  # @param [Float] amount to remove
+  # @return [none]
+  def remove_credit(amount_to_remove)
+    sql = "UPDATE store_credits SET amount = (amount - #{amount_to_remove}) WHERE id = #{self.id}"
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
