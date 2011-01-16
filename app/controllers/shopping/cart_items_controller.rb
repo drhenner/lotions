75271f70dc285d47260362efde1edbbf39d73648
David Henner <davidhenner@drhenner.local>
2011-01-16 18:41:40 +0000

    change look of product page and pass in qty to cart form

--- app/controllers/shopping/cart_items_controller.rb
+++ app/controllers/shopping/cart_items_controller.rb
@@ -44,7 +44,8 @@ class Shopping::CartItemsController < Shopping::BaseController
   def create
     @cart_item = get_new_cart_item
     session_cart.save if session_cart.new_record?
-    if cart_item = session_cart.add_variant(params[:cart_item][:variant_id], most_likely_user)
+    qty = params[:cart_item][:quantity].to_i
+    if cart_item = session_cart.add_variant(params[:cart_item][:variant_id], most_likely_user, qty)
       flash[:notice] = [I18n.t('out_of_stock_notice'), I18n.t('item_saved_for_later')].compact.join(' ') unless cart_item.shopping_cart_item?
       session_cart.save_user(most_likely_user)
       redirect_to(shopping_cart_items_url)
